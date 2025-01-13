fa8a22815738185b337677754d620f4184a87af3
mihguelaraujo <mihguelaraujo@gmail.com>
2025-01-13 13:49:47 -0300

    Estruturando o servidor com express e ejs, para renderizar automaticamente e de maneira poderosa o primeiro projeto

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+    res.render('index', {nome: 'Mihguel'});
+});
 
 app.listen(5000, () => {
     console.log('Servidor está rodando');
